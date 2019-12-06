38c17ab50eed3cb901b8f855dac4798b1a2d0118
Alp Sahin <alpsahin695@gmail.com>
2019-12-05 23:18:17 -0500

    Data Reading and Basics

---
+++ NB_and_SVM_practice_code.py
@@ -0,0 +1,28 @@
+# Import:
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+from dataset import Modules
+
+# Get training data:
+module_train = Modules(subset='train', shuffle=True)
+
+# Set NB Pipeline:
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+
+text_clf = text_clf.fit(module_train.description, module_train.label)
+# Get testing data:
+module_test = Modules(subset='test', shuffle=True)
+predicted = text_clf.predict(module_test.description)
+print(np.mean(predicted == module_test.label))
+
+# Set SVM Pipeline
+text_clf_svm = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),
+('clf-svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter_no_change=5, random_state=42))])
+
+text_clf_svm = text_clf_svm.fit(module_train.description, module_train.label)
+predicted_svm = text_clf_svm.predict(module_test.description)
+print(np.mean(predicted_svm == module_test.label))
---
+++ dataset.py
@@ -0,0 +1,77 @@
+# Importing:
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+
+# Class for modules (course names, course codes, course descriptions, labels)
+class Modules:
+
+    # Can provide training set, testing set or all, can shuffle
+    def __init__(self, subset = 'all', shuffle = True):
+        self.title = list()
+        self.code = list()
+        self.description = list()
+        self.label = list()
+
+        # Open and read json file into lists:
+        with open('test.json') as json_file:
+            data = json.load(json_file)
+            for p in data['table']:
+                self.title.append(p['title'])
+                self.code.append(p['code'])
+                self.description.append(p['description'])
+                self.label.append(p['label'])
+
+        # Shuffle on request:
+        if shuffle == True:
+            self.shuffle_data()
+        else:
+            pass
+
+        # Data seperation:
+        if subset == 'train':
+            self.seperate_data(0.6)
+        elif subset == 'test':
+            self.seperate_data(0.4)
+        else:
+            pass
+
+          
+    def shuffle_data(self):
+
+        # Keep corresponding title, code, descriptions and labels matched
+        order = np.arange(0,len(self.title))
+        np.random.shuffle(order)
+        title_ = list()
+        code_ = list()
+        description_ = list()
+        label_ = list()
+        for i in order:
+            title_.append(self.title[i])
+            code_.append(self.code[i])
+            description_.append(self.description[i])
+            label_.append(self.label[i])
+
+        self.title = title_
+        self.code = code_
+        self.description = description_
+        self.label = label_
+
+    def seperate_data(self,percent):
+
+        # Seperate data with given percentage:
+        title_ = list()
+        code_ = list()
+        description_ = list()
+        label_ = list()
+        for i in range(int(percent*len(self.title))):
+            title_.append(self.title[i])
+            code_.append(self.code[i])
+            description_.append(self.description[i])
+            label_.append(self.label[i])
+
+        self.title = title_
+        self.code = code_
+        self.description = description_
+        self.label = label_
